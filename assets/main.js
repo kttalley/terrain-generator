c94f37cb65f20f0e3302f47d618de9ed6b0126de
kttalley <talleykristian@gmail.com>
2019-03-17 22:29:44 -0800

    name change

--- assets/main.js
+++ assets/main.js
@@ -8,17 +8,20 @@ let orthoX;
 let orthoY;
 let orthoZ = w*w+4096;
 
+//terrain tiles
 let water;
 let sand;
 let grass;
 let rock;
 let dirt;
 let snow;
+let lava;
 
 function preload() {
-    
-    // grass = loadImage("grass.png");
+    sand  = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/sand.png")
+    water = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/water.jpg")    
     grass = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/grass.png");
+
 }
 
 function setup() {
