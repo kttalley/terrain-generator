6442b6fd0b41e9e24e6951cf90ea3f2e7c0aee88
kttalley <talleykristian@gmail.com>
2019-03-17 22:36:48 -0800

    render test 1

--- assets/main.js
+++ assets/main.js
@@ -12,28 +12,43 @@ let orthoZ = w*w+4096;
 let water;
 let sand;
 let grass;
-let rock;
+let stone;
 let dirt;
 let snow;
 let lava;
 
-function preload() {
-    sand  = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/sand.png")
-    water = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/water.jpg")    
-    grass = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/grass.png");
 
+function preload() {
+    water = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/water.jpg");  
+    sand  = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/sand.png");
+    grass = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/grass.png");
+    stone = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/stone.png");
+    dirt = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/dirt.jpg");
+    snow = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/snow.jpg");
+    lava = loadImage("https://raw.githubusercontent.com/kttalley/terrain-generator/master/assets/lava.jpg");
 }
 
 function setup() {
-    createCanvas(500, 500, WEBGL);
+    createCanvas(750, 500, WEBGL);
+    magicAngle = atan(1 /sqrt(2));
     orthoX = width;
     orthoY = height;
 }
 
 function draw() {
     background(200,200,0);
+    //grass
+    translate(-100,0, -100);
     rotateX(frameCount*0.01);
     texture(grass);
-    box(200, 200, 200);
+    box(50, 50, 50);
+    resetMatrix();
+
+     //snow
+     translate(100,0, -100);
+     rotateX(frameCount*0.01);
+     texture(snow);
+     box(50, 50, 50);
+     resetMatrix();
 
 }
